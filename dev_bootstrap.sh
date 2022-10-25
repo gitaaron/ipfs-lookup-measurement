303bb11f6ce3c1387fbbef3f7c946a3a48549e3d
Aaron Surty <aaron.surty@gmail.com>
2022-10-25 10:58:05 -0400

    FIX: docker controller using kubo version

--- dev_bootstrap.sh
+++ dev_bootstrap.sh
@@ -1,4 +1,4 @@
-CONTROLLER_ID=$(docker exec -it controller /app/go-ipfs/cmd/ipfs/ipfs id -f="<id>")
+CONTROLLER_ID=$(docker exec -it controller /app/kubo/cmd/ipfs/ipfs id -f="<id>")
 docker exec -it node0 /app/kubo/cmd/ipfs/ipfs bootstrap add /dns4/controller/tcp/4100/p2p/$CONTROLLER_ID
 docker exec -it node1 /app/kubo/cmd/ipfs/ipfs bootstrap add /dns4/controller/tcp/4100/p2p/$CONTROLLER_ID
 docker exec -it node2 /app/kubo/cmd/ipfs/ipfs bootstrap add /dns4/controller/tcp/4100/p2p/$CONTROLLER_ID
